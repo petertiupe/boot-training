ba0a73777e729599b77f6bcd4fd52cb83731e791
peter <peter@tiupe.de>
2017-12-07 09:39:44 +0100

    initial commit Spring Boot Schulung

---
+++ src/main/java/de/tiupe/boottraining/BootTrainingApplication.java
@@ -0,0 +1,12 @@
+package de.tiupe.boottraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTrainingApplication.class, args);
+	}
+}
